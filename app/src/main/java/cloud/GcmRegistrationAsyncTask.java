4a923c79520222e47e88cd2ee6b3c4550f7ce30d
ewedward <edward@ewins.com.tw>
2015-03-18 16:43:45 +0800

    Initial commit

---
+++ app/src/main/java/cloud/GcmRegistrationAsyncTask.java
@@ -0,0 +1,78 @@
+package cloud;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.redplayapp.gcm.registration.Registration;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by Edward on 2015/3/11.
+ */
+public class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
+    private static Registration regService = null;
+    private GoogleCloudMessaging gcm;
+    private Context context;
+
+    // TODO: change to your own sender ID to Google Developers Console project number, as per instructions above
+    private static final String SENDER_ID = "213637027462";
+
+    public GcmRegistrationAsyncTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        if (regService == null) {
+          /*  Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // Need setRootUrl and setGoogleClientRequestInitializer only for local testing,
+                    // otherwise they can be skipped
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
+                                throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+           */ // end of optional local run code
+            Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://majestic-poetry-88004.appspot.com/_ah/api/"); //http://3.majestic-poetry-88004.appspot.com/
+            regService = builder.build();
+        }
+
+        String msg = "";
+        try {
+            if (gcm == null) {
+                gcm = GoogleCloudMessaging.getInstance(context);
+            }
+            String regId = gcm.register(SENDER_ID);
+            msg = "Device registered, registration ID=" + regId;
+
+            // You should send the registration ID to your server over HTTP,
+            // so it can use GCM/HTTP or CCS to send messages to your app.
+            // The request to your server should be authenticated if your app
+            // is using accounts.
+            regService.register(regId).execute();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            msg = "Error: " + ex.getMessage();
+        }
+        return msg;
+    }
+
+    @Override
+    protected void onPostExecute(String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+        Logger.getLogger("REGISTRATION").log(Level.INFO, msg);
+    }
+}
---
+++ app/src/main/java/com/redplayapp/gae/MainActivity.java
@@ -0,0 +1,44 @@
+package com.redplayapp.gae;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import cloud.GcmRegistrationAsyncTask;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //new ServletPostAsyncTask().execute(new Pair<Context, String>(this, "Edward Manfred"));
+
+        //new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "edward end Manfred"));
+
+        new GcmRegistrationAsyncTask(this).execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
